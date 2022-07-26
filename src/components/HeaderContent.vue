1e45300e228cef6f855950cc2b50b3db3dc68bcb
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-07-26 16:12:01 +0200

    create compenent headercontent

---
+++ src/components/HeaderContent.vue
@@ -0,0 +1,31 @@
+<template>
+    <li class="d-inline px-3"> 
+        <a class="ms_color text-decoration-none" href="#"
+            :class="{'ms_new': this.new == true}">
+
+            {{ title }}
+
+        </a>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        new: Boolean,
+    },
+}
+</script>
+
+<style lang="scss">
+@import'../styles/variables.scss';
+
+.ms_color{
+    color: $textColor;
+}
+.ms_new{
+
+}
+
+</style>
