588f329efa9b54faa88a0c425c6af121b9650896
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-07-26 18:01:18 +0200

    static structure

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,243 @@
 <template>
-  <main>
+    <main class="container-fluid">
+        <div class="container p-2">
+            <img src="../assets/images/MasterStudy_logo.svg" alt="">
+            <p><i class="fa-solid fa-bars"></i> Category</p>
+            <!-- COMPONENT SEARCHBAR -->
+            <input type="text" placeholder="Search courses">
+            <button>Cerca</button>
+            <!-- COMPONENT SEARCHBAR -->
 
-  </main>
+            <p><i class="fa-solid fa-bullhorn"></i> Become an Instructor</p>
+            <p><i class="fa-solid fa-briefcase"></i> For Enterprise</p>
+            <p><i class="fa-solid fa-user"></i> Log In</p>
+
+            <!-- COMPONENT BUTTON -->
+            <button>SIGN</button>
+            <!-- COMPONENT BUTTON -->
+
+            <i class="fa-solid fa-bookmark"></i>
+        </div>
+
+        <div class="container p-2">
+            <div class="row">
+                <div class="col-7">
+                    <h1>TESTO</h1>
+                    <p>Altro testo</p>
+
+                    <!-- COMPONENT BUTTON -->
+                    <button>JOIN</button>
+                    <!-- COMPONENT BUTTON -->
+                </div>
+                <div class="col-5">
+                    <img src="../assets/images/base.png" alt="">
+                </div>
+            </div>
+        </div>
+
+        <div class="container p-2">
+            <div>
+                <i class="fa-solid fa-earth-europe"></i>
+                <h3>Titolino</h3>
+                <p>testo</p>
+            </div>
+            <div>
+                <i class="fa-solid fa-tv"></i>
+                <h3>Titolino</h3>
+                <p>testo</p>
+            </div>
+            <div>
+                <i class="fa-solid fa-calendar-check"></i>
+                <h3>Titolino</h3>
+                <p>testo</p>
+            </div>
+        </div>
+
+        <div class="container p-2">
+            <div class="row">
+                <div class="col-10">
+                    <img src="../assets/images/cat_1-1540x750.jpg" alt="">
+                    <h6>titolo</h6>
+                    <p>testo</p>
+                </div>
+                <div class="col-2">
+                    <div>
+                        <img src="../assets/images/cat_2-272x161.jpg" alt="">
+                        <h6>titolo</h6>
+                        <p>testo</p>
+                    </div>
+                    <div>
+                        <img src="../assets/images/cat_3-370x155.jpg" alt="">
+                        <h6>titolo</h6>
+                        <p>testo</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-4">
+                    <img src="../assets/images/cat_4-370x155.jpg" alt="">
+                    <h6>titolo</h6>
+                    <p>testo</p>
+                </div>
+                <div class="col-4">
+                    <img src="../assets/images/cat_5-370x155.jpg" alt="">
+                    <h6>titolo</h6>
+                    <p>testo</p>
+                </div>
+                <div class="col-4">
+                    <img src="../assets/images/cat_6-370x155.jpg" alt="">
+                    <h6>titolo</h6>
+                    <p>testo</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="container p-2">
+            <div class="row">
+                <div class="col-7">
+                    <h1>TESTO</h1>
+                    <p>Altro testo</p>
+
+                    <!-- COMPONENT BUTTON -->
+                    <button>JOIN</button>
+                    <!-- COMPONENT BUTTON -->
+                </div>
+                <div class="col-5">
+                    <img src="../assets/images/Untitled-1-1-1-1-1.png" alt="">
+                </div>
+            </div>
+        </div>
+
+        <div class="container p-2">
+            <h1>Recent courses</h1>
+            <!-- COMPONENT GENERE CORSI -->
+            <div>
+                <ul>
+                    <li>a</li>
+                    <li>b</li>
+                    <li>c</li>
+                    <li>d</li>
+                    <li>e</li>
+                    <li>f</li>
+                    <li>g</li>
+                </ul>
+            </div>
+            <!-- COMPONENT GENERE CORSI -->
+
+            <!-- COMPONENT CARD CORSI -->
+            <div>
+                <div class="row">
+                    <div class="col-2">
+                        <img src="../assets/images/photo-1416339134316-0e91dc9ded92-scaled-272x161.jpeg" alt="">
+                        <p>descrizione</p>
+                        <h6>Titolino</h6>
+                        <span>ore</span>
+                        <span>costo</span>
+                    </div>
+
+                    <div class="col-2">
+                        <img src="../assets/images/photo-1416339134316-0e91dc9ded92-scaled-272x161.jpeg" alt="">
+                        <p>descrizione</p>
+                        <h6>Titolino</h6>
+                        <span>ore</span>
+                        <span>costo</span>
+                    </div>
+
+                    <div class="col-2">
+                        <img src="../assets/images/photo-1416339134316-0e91dc9ded92-scaled-272x161.jpeg" alt="">
+                        <p>descrizione</p>
+                        <h6>Titolino</h6>
+                        <span>ore</span>
+                        <span>costo</span>
+                    </div>
+
+                    <div class="col-2">
+                        <img src="../assets/images/photo-1416339134316-0e91dc9ded92-scaled-272x161.jpeg" alt="">
+                        <p>descrizione</p>
+                        <h6>Titolino</h6>
+                        <span>ore</span>
+                        <span>costo</span>
+                    </div>
+
+                    <div class="col-2">
+                        <img src="../assets/images/photo-1416339134316-0e91dc9ded92-scaled-272x161.jpeg" alt="">
+                        <p>descrizione</p>
+                        <h6>Titolino</h6>
+                        <span>ore</span>
+                        <span>costo</span>
+                    </div>
+
+                    <div class="col-2">
+                        <img src="../assets/images/photo-1416339134316-0e91dc9ded92-scaled-272x161.jpeg" alt="">
+                        <p>descrizione</p>
+                        <h6>Titolino</h6>
+                        <span>ore</span>
+                        <span>costo</span>
+                    </div>
+                </div>
+
+                                <div class="row">
+                    <div class="col-2">
+                        <img src="../assets/images/photo-1416339134316-0e91dc9ded92-scaled-272x161.jpeg" alt="">
+                        <p>descrizione</p>
+                        <h6>Titolino</h6>
+                        <span>ore</span>
+                        <span>costo</span>
+                    </div>
+
+                    <div class="col-2">
+                        <img src="../assets/images/photo-1416339134316-0e91dc9ded92-scaled-272x161.jpeg" alt="">
+                        <p>descrizione</p>
+                        <h6>Titolino</h6>
+                        <span>ore</span>
+                        <span>costo</span>
+                    </div>
+
+                    <div class="col-2">
+                        <img src="../assets/images/photo-1416339134316-0e91dc9ded92-scaled-272x161.jpeg" alt="">
+                        <p>descrizione</p>
+                        <h6>Titolino</h6>
+                        <span>ore</span>
+                        <span>costo</span>
+                    </div>
+
+                    <div class="col-2">
+                        <img src="../assets/images/photo-1416339134316-0e91dc9ded92-scaled-272x161.jpeg" alt="">
+                        <p>descrizione</p>
+                        <h6>Titolino</h6>
+                        <span>ore</span>
+                        <span>costo</span>
+                    </div>
+
+                    <div class="col-2">
+                        <img src="../assets/images/photo-1416339134316-0e91dc9ded92-scaled-272x161.jpeg" alt="">
+                        <p>descrizione</p>
+                        <h6>Titolino</h6>
+                        <span>ore</span>
+                        <span>costo</span>
+                    </div>
+
+                    <div class="col-2">
+                        <img src="../assets/images/photo-1416339134316-0e91dc9ded92-scaled-272x161.jpeg" alt="">
+                        <p>descrizione</p>
+                        <h6>Titolino</h6>
+                        <span>ore</span>
+                        <span>costo</span>
+                    </div>
+                </div>
+                
+                <!-- TENDINA LATERALE -->
+                <div></div>
+                <!-- TENDINA LATERALE -->
+
+                <!-- COMPONENT BUTTON -->
+                <button>JOIN</button>
+                <!-- COMPONENT BUTTON -->
+            </div>
+            <!-- COMPONENT CARD CORSI -->
+        </div>
+    </main>
 </template>
 
 <script>
