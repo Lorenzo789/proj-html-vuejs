26f8c6c3b7499d2488c1a74cb870e453215a6f5b
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-07-26 13:36:46 +0200

    create component macro

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-
+    <!-- 26/07/22 FINIRE HEADER E MINIMO UN PEZZO MAIN CSS INCLUSO -->
+    <Header />
+    <Main />
+    <Footer />
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
 
 
 export default {
   name: 'App',
   components: {
-    
-  }
+    Header,
+    Main,
+    Footer
+  },
 }
 </script>
 
