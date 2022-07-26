80d2d5a0434fabf98b69d5a5afc79b6b17e9530d
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-07-26 18:27:08 +0200

    footer structure static

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,69 @@
 <template>
-  <footer>
+    <footer class="container-fluid">
+        <div class="container p-2">
+            <div class="row">
+                <!-- COMPONENT -->
+                <div class="col-3">About
+                    <p>testo</p>
+                    <span>icona</span>
+                    <span>icona</span>
+                    <span>icona</span>
+                </div>
+                <!-- COMPONENT -->
 
-  </footer>
+                <!-- COMPONENT -->
+                <div class="col-3">Contact
+                    <div>
+                        indirizzo
+                    </div>
+                    <div>
+                        contatti
+                    </div>
+                    <div>
+                        mail
+                    </div>
+                </div>
+                <!-- COMPONENT -->
+
+                <!-- COMPONENT -->
+                <div class="col-3">Pages
+                    <ul>
+                        <li>a</li>
+                        <li>b</li>
+                        <li>c</li>
+                        <li>d</li>
+                        <li>e</li>
+                        <li>f</li>
+                    </ul>
+                </div>
+                <!-- COMPONENT -->
+
+                <!-- COMPONENT -->
+                <div class="col-3">Blog
+                    <div class="row">
+                        <div class="col-6">
+                            immagine<img src="" alt="">
+                        </div>
+                        <div class="col-6">
+                            <p>testo immagine</p>
+                            <span>data</span>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-6">
+                            immagine<img src="" alt="">
+                        </div>
+                        <div class="col-6">
+                            <p>testo immagine</p>
+                            <span>data</span>
+                        </div>
+                    </div>
+                </div>
+                <!-- COMPONENT -->
+            </div>
+        </div>
+    </footer>
 </template>
 
 <script>
@@ -12,5 +74,4 @@ export default {
 
 <style lang="scss" scoped>
 @import'../styles/variables.scss';
-
 </style>
