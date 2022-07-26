a8f85f47de705ca6ccde43a2004aaf0c987f914a
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-07-26 16:12:17 +0200

    population headercontent

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,78 @@
 <template>
-  <header>
-    
+  <header class="container-fluid">
+    <div class="container d-flex justify-content-between">
+        <ul>
+            <li class="ms_color">English <i class="fa-solid fa-angle-down"></i> </li>
+        </ul>
+
+        <div>
+            <ul>
+                <HeaderContent v-for="(data, index) in headerData" :key="index"
+                    :title = "data.text"
+                    :new = "data.new"
+                />
+                <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-behance"></i></a>
+                <a href="#"><i class="fa-solid fa-basketball"></i></a>
+                <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                <a href="#"><i class="fa-brands fa-twitter"></i></a>
+            </ul>
+        </div>
+
+    </div>
   </header>
 </template>
 
 <script>
-export default {
+import HeaderContent from './HeaderContent.vue';
 
+export default {
+    components: { HeaderContent,},
+    data: function(){
+        return{
+            headerData: [
+                {
+                    text: 'Courses',
+                    new: false,
+                },
+                {
+                    text: 'Zoom',
+                    new: true,
+                },
+                {
+                    text: 'Pages',
+                    new: false,
+                },
+                {
+                    text: 'Bundles',
+                    new: false,
+                },
+                {
+                    text: 'Course Formats',
+                    new: false,
+                },
+                {
+                    text: 'Pages',
+                    new: false,
+                },
+                {
+                    text: 'Add Course',
+                    new: true,
+                },
+                {
+                    text: 'Demos',
+                    new: false,
+                },
+            ],
+        }
+    }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import'../styles/variables.scss';
 
 </style>
