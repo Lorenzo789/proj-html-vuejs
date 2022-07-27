1b1e6a0b411ce94f0ac0abe8a394a0bac54876ce
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-07-28 00:52:04 +0200

    card done

--- src/components/ComponentsMain/CardGenre.vue
+++ src/components/ComponentsMain/CardGenre.vue
@@ -6,9 +6,35 @@
 
         <div class="p-3">
             <p>{{ description }}</p>
-            <h6>{{ title }}</h6>
-            <span>{{ hour }} {{ stars }}</span>
-            <span>{{ cost }}</span>
+            <h6 class="fs-6">{{ title }}</h6>
+            <hr>
+
+            <div class="position-relative d-flex justify-content-between">
+                
+                <div>
+                    <span>
+                        <i class="p-2 fa-solid" :class="{'fa-clock': this.hour !== ''}"></i>{{ hour }}
+                    </span>
+    
+                    <span class="ms_star">
+                        {{ stars }}      
+                    </span>
+    
+                    <span>
+                        {{ numberStars }}      
+                    </span>
+                </div>
+
+                <span class="position-absolute ms_pos">
+                    {{ oldCost }}
+                </span>
+
+                <span class="fs-6 fw-bold">
+                    {{ cost }}
+                </span>
+
+            </div>
+            
         </div>
 
     </div>
@@ -21,19 +47,23 @@ export default {
         description: String,
         title: String,     
         hour: String,
-        stars: String,       
-        cost:  String,    
+        stars: String,
+        numberStars: String,       
+        cost:  String,
+        oldCost: String,    
         new: Boolean,     
         hot: Boolean,     
         special: Boolean,
-    }
+    },
 }
 </script>
 
 <style lang="scss" scoped>
+@import'../../styles/variables.scss';
 
     .ms_courses_card{
         width: calc(100% / 6 - 20px);
+        height: 350px;
         margin-bottom: 60px;
         border: 2px solid lightgray;
 
@@ -41,6 +71,23 @@ export default {
             object-fit: cover;
             height: 100%;
             width: 100%;
+        }
+
+        p{
+            color: #b9b4b2;
+            font-size: 13px;
+        }
+
+        .ms_star{
+            color: #efb467;
+            font-size: 18px;
+        }
+
+        .ms_pos{
+            top: -15px;
+            right: 0;
+            text-decoration-line: line-through;
+            color: lightgray;
         }
     }
 </style>
