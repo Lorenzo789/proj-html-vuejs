685199e7ecf9d08a2853bc251ca15b9248d30e55
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-07-27 10:32:54 +0200

    button dynamic

--- src/components/ButtonComponent.vue
+++ src/components/ButtonComponent.vue
@@ -1,17 +1,29 @@
 <template>
   <div>
     <!-- COMPONENT BUTTON -->
-    <button>SIGN</button>
+    <button>{{ text }}</button>
     <!-- COMPONENT BUTTON -->
   </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    text: String,
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 @import'../styles/variables.scss';
+
+button{
+  background-color: $colorButton;
+  border-color: $colorButton;
+  color: white;
+  padding: 15px 40px;
+  border-radius: 30px;
+  font-weight: bold;
+  text-transform: uppercase;
+}
 </style>
